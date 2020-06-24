f31ef41d6a9f6b0a947f054cb7b5977090cfb1d5
njbsb <49741741+njbsb@users.noreply.github.com>
2020-06-24 19:52:37 +0800

    listener for change in SharedPreference added ui for wind, humidity, pressure and uv index

--- app/src/main/java/com/fyp/kellyweatherapp/fragment/HomeFragment.java
+++ app/src/main/java/com/fyp/kellyweatherapp/fragment/HomeFragment.java
@@ -1,8 +1,11 @@
 package com.fyp.kellyweatherapp.fragment;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -16,9 +19,12 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.fyp.kellyweatherapp.activity.MainActivity;
 import com.fyp.kellyweatherapp.database.PrefConfig;
 import com.fyp.kellyweatherapp.R;
 import com.fyp.kellyweatherapp.adapter.WeatherDailyAdapter;
@@ -28,6 +34,9 @@ import com.fyp.kellyweatherapp.model.POJO.CurrentWeatherData;
 import com.fyp.kellyweatherapp.model.POJO.Daily;
 import com.fyp.kellyweatherapp.model.POJO.WeatherData;
 import com.fyp.kellyweatherapp.model.User;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.gson.Gson;
 import com.squareup.picasso.Picasso;
 
@@ -42,15 +51,17 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class HomeFragment extends Fragment implements View.OnClickListener {
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
+
+public class HomeFragment extends Fragment implements View.OnClickListener, SharedPreferences.OnSharedPreferenceChangeListener {
 
     // UI components
     private RecyclerView recyclerView;
     private ProgressBar progressbarHome;
     private ImageButton refresh;
     private ImageView icon;
-    private TextView tv_city, tv_coordinate, tv_condition, tv_temperature;
-    // Objects
+    private TextView tv_city, tv_coordinate, tv_condition, tv_temperature, tv_wind, tv_pressure, tv_humidity, tv_uvindex;
+    // Object
     private View view;
     private ArrayList<Daily> dailies;
     // POJO
@@ -77,40 +88,47 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
         tv_coordinate = view.findViewById(R.id.txt_coordinate);
         tv_condition = view.findViewById(R.id.txt_cond);
         tv_temperature = view.findViewById(R.id.txt_temp);
+        tv_wind = view.findViewById(R.id.txt_windtext);
+        tv_pressure = view.findViewById(R.id.txt_pressure);
+        tv_humidity = view.findViewById(R.id.txt_humidity);
+        tv_uvindex = view.findViewById(R.id.txt_uvitext);
+
         recyclerView = view.findViewById(R.id.home_recycler);
 
-        latitude = PrefConfig.loadLatitude(Objects.requireNonNull(getContext()));
-        longitude = PrefConfig.loadLongitude(Objects.requireNonNull(getContext()));
-        if(latitude.equals("") || longitude.equals("")) {
-            shortToast("Default location at 0,0");
-        }
-        // Check if CurrentWeatherData refresh only when date is different
-
-//        SharedPreferences preferences = getContext().getSharedPreferences("com.fyp.kellyweatherapp", Context.MODE_PRIVATE);
-//        Map<String, ?> alldata = preferences.getAll();
-//        shortToast("alldata size: " + alldata.size());
-        if(PrefConfig.loadWeatherData(getContext()) == null
-                || PrefConfig.loadCurrentWeatherData(getContext()) == null) {
-            shortToast("No data in local storage");
-            Log.d(TAG, "onCreateView: PrefConfig WD and CWD is null");
-            getAPIdata();
-        }
-        else {
-            shortToast("Showing data from local storage");
-            Log.d(TAG, "onCreateView: PrefConfig WD and CWD are not null");
-            Date date_today = Calendar.getInstance().getTime();
-            Date date_inPref = PrefConfig.loadWeatherData(getContext()).getDaily().get(0).getDateasDate();
-            boolean dateUptoDate = compareDates(date_today, date_inPref);
-            if(dateUptoDate) {
-                getAPIdata();
-            }
-            loadHomeDataWD(PrefConfig.loadWeatherData(getContext()));
-            loadHomeDataCWD(PrefConfig.loadCurrentWeatherData(getContext()));
-            progressbarHome.setVisibility(View.INVISIBLE);
-        }
-//        shortToast(PrefConfig.loadUser(getContext()).getNotesList().toString());
+//        loadUIData();
         refresh.setOnClickListener(this);
         return view;
+    }
+
+    private void loadUIData() {
+        latitude = PrefConfig.loadLatitude(Objects.requireNonNull(getContext()));
+        longitude = PrefConfig.loadLongitude(Objects.requireNonNull(getContext()));
+        if(latitude.equals("0") || longitude.equals("0")) {
+            getLocationData();
+            latitude = PrefConfig.loadLatitude(Objects.requireNonNull(getContext()));
+            longitude = PrefConfig.loadLongitude(Objects.requireNonNull(getContext()));
+        }
+        else {
+            if(PrefConfig.loadWeatherData(getContext()) == null
+                    || PrefConfig.loadCurrentWeatherData(getContext()) == null) {
+                shortToast("Fetching data from API");
+                Log.d(TAG, "onCreateView: PrefConfig WD and CWD is null");
+                getAPIdata();
+            }
+            else {
+                shortToast("Showing data from local storage");
+                Log.d(TAG, "onCreateView: PrefConfig WD and CWD are not null");
+                Date date_today = Calendar.getInstance().getTime();
+                Date date_inPref = PrefConfig.loadWeatherData(getContext()).getDaily().get(0).getDateasDate();
+                boolean dateUptoDate = compareDates(date_today, date_inPref);
+                if(dateUptoDate) {
+                    getAPIdata();
+                }
+                loadHomeDataWD(PrefConfig.loadWeatherData(getContext()));
+                loadHomeDataCWD(PrefConfig.loadCurrentWeatherData(getContext()));
+            }
+            progressbarHome.setVisibility(View.INVISIBLE);
+        }
     }
 
     private void disableUI() {
@@ -135,6 +153,12 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
     @Override
     public void onStart() {
         super.onStart();
+        if(PrefConfig.loadLatitude(Objects.requireNonNull(getContext())).equals("0") || PrefConfig.loadLongitude(getContext()).equals("0")) {
+            getLocationData();
+        }
+        else {
+            loadUIData();
+        }
     }
     private static Date setTimeToMidnight(Date date) {
         Calendar calendar = Calendar.getInstance();
@@ -153,13 +177,34 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
         return todayDate.after(dbDate);
     }
 
-    private void getTodaysData() {
-
+    private void getLocationData() {
+        if(PrefConfig.loadLatitude(Objects.requireNonNull(getContext())).equals("0") || PrefConfig.loadLongitude(getContext()).equals("0")) {
+            ActivityCompat.requestPermissions(Objects.requireNonNull(getActivity()), new String[]{ACCESS_FINE_LOCATION}, 1);
+            if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                    && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                ((MainActivity)getActivity()).requestRuntimePermissionLocation();
+                return;
+            }
+            FusedLocationProviderClient fusedLocationClient = LocationServices.getFusedLocationProviderClient(getActivity());
+            fusedLocationClient.getLastLocation().addOnSuccessListener(new OnSuccessListener<Location>() {
+                @Override
+                public void onSuccess(Location location) {
+                    String latitude = String.valueOf(location.getLatitude());
+                    String longitude = String.valueOf(location.getLongitude());
+                    PrefConfig.saveLatitude(Objects.requireNonNull(getContext()), latitude);
+                    PrefConfig.saveLongitude(getContext(), longitude);
+                    loadUIData();
+                }
+            });
+        }
+        // no else block bcs latitude & longitude will not be used in MainActivity UI
     }
 
     private void getAPIdata() {
-
         Service serviceAPI = Client.getClient(baseURL);
+//        ((MainActivity) Objects.requireNonNull(getActivity())).getLocation();
+        latitude = PrefConfig.loadLatitude(Objects.requireNonNull(getContext()));
+        longitude = PrefConfig.loadLongitude(getContext());
         Call<WeatherData> weeklyCall = serviceAPI.getWeeklyWeather(latitude, longitude, exclude, unit, APIkey);
         Call<CurrentWeatherData> currentCall = serviceAPI.getCurrentWeather(latitude, longitude, unit, APIkey);
         weeklyCall.enqueue(new Callback<WeatherData>() {
@@ -200,8 +245,36 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
         });
     }
 
+    private String getUVdescription(double uvi) {
+        String desc = "";
+        if(uvi<3)
+            desc = "Low";
+        else if(uvi>=3 && uvi<=5)
+            desc = "Moderate";
+        else if(uvi>=6 && uvi<=7)
+            desc = "High";
+        else if(uvi>=8 && uvi<=10)
+            desc = "Very High";
+        else
+            desc = "Very High";
+        return desc;
+    }
+
     private void loadHomeDataWD(WeatherData weatherData) {
         dailies = (ArrayList<Daily>) weatherData.getDaily();
+
+        String windspeed = String.valueOf(weatherData.getCurrent().getWindSpeed());
+        String humidity = String.valueOf(weatherData.getCurrent().getHumidity());
+        String pressure = String.valueOf(weatherData.getCurrent().getPressure());
+        double uvi = weatherData.getCurrent().getUvi();
+        String uvindex = String.valueOf(uvi);
+        String uvindex_desc = getUVdescription(uvi);
+
+        tv_wind.setText(String.format("%s m/s", windspeed));
+        tv_humidity.setText(String.format("%s %%", humidity));
+        tv_pressure.setText(String.format("%s hPa", pressure));
+        tv_uvindex.setText(String.format("%s (%s)", uvindex, uvindex_desc));
+
         initializeRecycler(dailies);
     }
 
@@ -212,6 +285,12 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
         String condition = currentWeatherData.getWeather().get(0).getMain();
         String temperature = String.valueOf(currentWeatherData.getMain().getTemp());
         String iconURL = currentWeatherData.getWeather().get(0).getIconURL();
+
+//        String windspeed = currentWeatherData.getWind().getSpeed().toString();
+//        String humidity = currentWeatherData.getMain().getHumidity().toString();
+//        String pressure = currentWeatherData.getMain().getPressure().toString();
+
+
         tv_city.setText(String.format("%s, %s", city, country));
         tv_coordinate.setText(coord);
         tv_condition.setText(condition);
@@ -241,6 +320,14 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
                 progressbarHome.setVisibility(View.INVISIBLE);
                 shortToast("Weather data refreshed");
                 break;
+        }
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+//        getAPIdata();
+        if(key.equals(PrefConfig.LAT) || key.equals(PrefConfig.LON)) {
+            getAPIdata();
         }
     }
 }
